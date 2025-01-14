4ca124832492c82693244efeecb8203433cc1fcd
Samet <samet.kilincarslan@sans-technology.com>
2025-01-15 00:42:04 +0300

    update index.kt

--- src/jsMain/kotlin/org/akilincarslan/ahrarwood/pages/Index.kt
+++ src/jsMain/kotlin/org/akilincarslan/ahrarwood/pages/Index.kt
@@ -1,11 +1,14 @@
 package org.akilincarslan.ahrarwood.pages
 
 import androidx.compose.runtime.*
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
 import com.varabyte.kobweb.core.Page
+import com.varabyte.kobweb.silk.components.graphics.Image
 import org.jetbrains.compose.web.dom.Text
 import com.varabyte.kobweb.worker.rememberWorker
 import org.akilincarslan.ahrarwood.worker.EchoWorker
@@ -18,8 +21,13 @@ fun HomePage() {
         worker.postInput("Hello, worker!")
     }
 
-    // TODO: Replace the following with your own content
     Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("THIS PAGE INTENTIONALLY LEFT BLANK")
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image("/icon.jpg", alt = "ahrarWood", width = 240, height = 240)
+            Text("Welcome to AhrarWood")
+        }
     }
 }
