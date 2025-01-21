8e8f4acb090d3d7e3201415b128cbe197a543c11
Samet <samet.kilincarslan@sans-technology.com>
2025-01-21 14:06:21 +0300

    fix gitignore

--- src/jsMain/kotlin/org/akilincarslan/ahrarwood/constants/StringKeys.kt
+++
@@ -1,8 +0,0 @@
-package org.akilincarslan.ahrarwood.constants
-
-object StringKeys {
-    const val HOME = "navigator_home"
-    const val ABOUT = "navigator_about"
-    const val CONTACT = "navigator_contact"
-    const val BOOK_STANDS = "navigator_book_stands"
-}
